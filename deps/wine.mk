fcf6c23c86689a71113fc03c2e5f6fef27907ad4
Gabriel Baraldi <baraldigabriel@gmail.com>
2022-07-27 15:31:11 -0300

    make it ALL caps

--- deps/wine.mk
+++ deps/wine.mk
@@ -2,7 +2,8 @@
 include $(SRCDIR)/wine.version
 
 ifeq ($(USE_BINARYBUILDER_WINE),1)
+BB_TRIPLET = x86_64-linux-gnu
+$(eval $(call bb-install,wine,WINE,false))
 
-$(eval $(call bb-install,wine,wine,false))
 
 endif
